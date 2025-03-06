575aeb33372463ba5eb92dcd17e5a1cd006eb5bb
phamquangha <phamquangha1611@gmail.com>
2025-03-06 19:49:43 +0700

    tìm số lớn nhất của x và y

--- phamha/main.c
+++ phamha/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 void main() {
-	printf("hello world \n");
+	int x = 0;
+	int y = 0;
+	printf("nhap gia tri cho x: ");
+	scanf_s("%d", &x);
+	printf("nhap gia tri cho y: ");
+	scanf_s("%d", &y);
+	if (x > y) {
+		printf(" gia tri lon nhat la x %d",x);
+	}
+	else {
+		printf(" gia tri lon nhat la y %d",y);
+	}
+		
 }
