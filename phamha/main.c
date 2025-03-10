3f2b837333c49aa2f7f33aa1636a107a824e584c
phamquangha <phamquangha1611@gmail.com>
2025-03-10 10:24:04 +0700

    bài 7 btvn

--- phamha/main.c
+++ phamha/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 void main() {
-	int x = 0;
-	printf("nhap vao gia tri cua x: \n");
-	scanf_s("%d", &x);
-	if (x % 2 == 0) {
-		printf(" day la so chan");
+	float x = 0;
+	float n = 0;
+	printf("nhap vao gia tri tien dien: ");
+	scanf_s("%f", &x);
+	if (x > 0 && x < 50) {
+		n = x * 1.678;
+		printf(" gia tri tien dien la: n %f", n);
+	}
+	else if (x > 51 && x < 100) {
+		n = x * 1.734;
+		printf(" gia tri tien dien la: n %f", n);
+	}
+	else if (x > 101 && x< 200) {
+		n = x * 2.014;
+		printf(" gia tri tien dien la: n %f", n);
+	}
+	else if (x>201 && x<300) {
+		n = x * 2.536;
+		printf(" gia tri tien dien la: n %f", n);
+	}
+	else if (x > 301 && x<400) {
+		n = x * 2.834;
+		printf(" gia tri tien dien la: n %f", n);
 	}
 	else {
-		printf(" day la so le");
+		n = x * 2.927;
+		printf("gia tri tien dien la: n %f", n);
 	}
 }
