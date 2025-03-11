4488f2b69b3d2208bd366893985fcb562a2054f1
phamquangha <phamquangha1611@gmail.com>
2025-03-11 12:35:08 +0700

    bài 7 btvn

--- phamha/main.c
+++ phamha/main.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 void main() {
-	float x = 0;
-	float n = 0;
-	printf("nhap vao gia tri tien dien: ");
-	scanf_s("%f", &x);
-	if (x > 0 && x < 50) {
-		n = x * 1.678;
-		printf(" gia tri tien dien la: n %f", n);
+	int TTCN = 0;
+	printf(" thu nhap ca nhan la: \n");
+	scanf_s("%d", &TTCN);
+	float thueTTCN = 0;
+	float thunhapchiuthue = 0;
+	if (thunhapchiuthue <= 5000000) {
+		thueTTCN = TTCN * 0.05;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f" , thueTTCN);
 	}
-	else if (x > 51 && x < 100) {
-		n = x * 1.734;
-		printf(" gia tri tien dien la: n %f", n);
+	else if (thunhapchiuthue > 5000000 && thunhapchiuthue <=  10000000) {
+		thueTTCN = TTCN * 0.1;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
 	}
-	else if (x > 101 && x< 200) {
-		n = x * 2.014;
-		printf(" gia tri tien dien la: n %f", n);
+	else if ( thunhapchiuthue > 10000000 && thunhapchiuthue <= 18000000) {
+		thueTTCN = TTCN * 0.15;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
 	}
-	else if (x>201 && x<300) {
-		n = x * 2.536;
-		printf(" gia tri tien dien la: n %f", n);
+	else if (thunhapchiuthue > 18000000 && thunhapchiuthue <= 32000000) {
+		thueTTCN = TTCN * 0.2;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f", thueTTCN);
 	}
-	else if (x > 301 && x<400) {
-		n = x * 2.834;
-		printf(" gia tri tien dien la: n %f", n);
+	else if (thunhapchiuthue > 32000000 && thunhapchiuthue <= 52000000) {
+		thueTTCN = TTCN * 0.25;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
+	}
+	else if ( thunhapchiuthue > 52000000 && thunhapchiuthue <= 80000000) {
+		thueTTCN = TTCN * 0.3;
+		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
 	}
 	else {
-		n = x * 2.927;
-		printf("gia tri tien dien la: n %f", n);
+		thueTTCN = TTCN * 0.35;
+		printf(" thue thu nhap ca nhan la: thue TTCN %f" , thueTTCN);
 	}
 }
