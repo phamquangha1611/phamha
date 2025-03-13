6f005b1f143059d460587e7c1fa12d1b994a9c2e
phamquangha <phamquangha1611@gmail.com>
2025-03-13 18:41:12 +0700

    bai 2; tính giai thừa

--- phamha/main.c
+++ phamha/main.c
@@ -1,36 +1,9 @@
 #include <stdio.h>
 void main() {
-	int TTCN = 0;
-	printf(" thu nhap ca nhan la: \n");
-	scanf_s("%d", &TTCN);
-	float thueTTCN = 0;
-	float thunhapchiuthue = 0;
-	if (thunhapchiuthue <= 5000000) {
-		thueTTCN = TTCN * 0.05;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f" , thueTTCN);
-	}
-	else if (thunhapchiuthue > 5000000 && thunhapchiuthue <=  10000000) {
-		thueTTCN = TTCN * 0.1;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
-	}
-	else if ( thunhapchiuthue > 10000000 && thunhapchiuthue <= 18000000) {
-		thueTTCN = TTCN * 0.15;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
-	}
-	else if (thunhapchiuthue > 18000000 && thunhapchiuthue <= 32000000) {
-		thueTTCN = TTCN * 0.2;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f", thueTTCN);
-	}
-	else if (thunhapchiuthue > 32000000 && thunhapchiuthue <= 52000000) {
-		thueTTCN = TTCN * 0.25;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
-	}
-	else if ( thunhapchiuthue > 52000000 && thunhapchiuthue <= 80000000) {
-		thueTTCN = TTCN * 0.3;
-		printf(" thue thu nhap ca nhan la: thueTTCN %f" ,thueTTCN);
-	}
-	else {
-		thueTTCN = TTCN * 0.35;
-		printf(" thue thu nhap ca nhan la: thue TTCN %f" , thueTTCN);
-	}
+	int c, n, tich = 1;
+	printf("nhap so de tinh giai thua\n");
+	scanf_s("%d", &n);
+	for (c = 1; c <= n; c++) tich = tich * c;
+	printf("giai thua cua %d = %d\n", n, tich);
+	return 0;
 }
