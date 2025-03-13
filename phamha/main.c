e5e4f91f8eb18e1c083d0683b3ba3f2bead5357c
phamquangha <phamquangha1611@gmail.com>
2025-03-13 18:46:53 +0700

    bài 1 tính tổng số nguyên

--- phamha/main.c
+++ phamha/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-void main() {
-	int c, n, tich = 1;
-	printf("nhap so de tinh giai thua\n");
-	scanf_s("%d", &n);
-	for (c = 1; c <= n; c++) tich = tich * c;
-	printf("giai thua cua %d = %d\n", n, tich);
-	return 0;
+int main() {
+	int n = 0;
+	printf("nhap gia tri cua n:");
+    scanf_s("%d", &n);
+	int tong = 0;
+	for (int i = 1; i <= n; i++) {
+		tong = tong + i;
+		
+	}
+	printf(" tong la: %d", tong);
 }
