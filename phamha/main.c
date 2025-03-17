51feb26480ed2ca9e2bc25505afef8b7b696a64d
phamquangha <phamquangha1611@gmail.com>
2025-03-17 09:25:37 +0700

    bài 2 mảng

--- phamha/main.c
+++ phamha/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 void main() {
-	int sum_arr = 0;
-	int arr[10];
-	for (int i = 0; i < 10; i++) {
-		printf(" nhap phan tu cua mang:  %d", i );
-		scanf_s("%d", & arr[i]);
-		sum_arr += arr[i];
+	int arr[10] ;
+	int trung_binh_cong = 0;
+	int tong = 0;
+	int dem = 0;
+	for (int i = 0;i< 10 ; i++) {
+		printf(" Nhap vao phan tu %d = ", i);
+		scanf_s("%d", &arr[i]);
+		if ( arr[i] %2 != 0) {
+			tong = arr[i] + tong;
+			dem++;
 		}
-	
-	printf(" tong la:  %d", sum_arr);
+		
 	}
-
+	trung_binh_cong = (double)tong / dem;
+	printf(" trung binh cong cua cac so le la: %d", trung_binh_cong);
+	}
