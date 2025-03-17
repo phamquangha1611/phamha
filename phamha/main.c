f56ec673bac50aba413c08ce15e986a3134fd00d
phamquangha <phamquangha1611@gmail.com>
2025-03-17 09:40:42 +0700

    bài 3 mảng

--- phamha/main.c
+++ phamha/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 void main() {
-	int arr[10] ;
-	int trung_binh_cong = 0;
-	int tong = 0;
-	int dem = 0;
-	for (int i = 0;i< 10 ; i++) {
-		printf(" Nhap vao phan tu %d = ", i);
-		scanf_s("%d", &arr[i]);
-		if ( arr[i] %2 != 0) {
-			tong = arr[i] + tong;
-			dem++;
+	int arr[] = { 1,2,3,4,4,5,5,3,2,2,4,5 };
+	int so_lon_nhat = 0;
+	int so_luong_doi_tuong = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < so_luong_doi_tuong; i++) {
+		if (so_lon_nhat < arr[i]) {
+			so_lon_nhat = arr[i];
 		}
-		
+		for (int i = 0; i < so_luong_doi_tuong; i++) {
+			if (arr[i] == so_lon_nhat)
+				printf("%d ", i);
+		}
+
 	}
-	trung_binh_cong = (double)tong / dem;
-	printf(" trung binh cong cua cac so le la: %d", trung_binh_cong);
-	}
+}
+	
