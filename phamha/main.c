8a76aed2b54d410f17b6f8ec0b70c61e21d1d6e4
phamquangha <phamquangha1611@gmail.com>
2025-03-18 18:28:08 +0700

    bài 8 mảng

--- phamha/main.c
+++ phamha/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 void main() {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int so_phan_tu_mang = sizeof(arr) / sizeof(arr[0]);
-	for (int i = 0; i < so_phan_tu_mang; i++) {
-		if (arr[i] % 2 != 0) {
-			printf(" cac gia tri le la: %d", arr[i]);
-		}
-		}
-		
+	int arr[] = { 1,2 };
+	int c = arr[0];
+	arr[0] = arr[1]; 
+	arr[1] = c;
+	printf("arr[0] = %d, arr[1] = %d ", arr[0], arr[1]);
 	}
+		
+	
+		
+	
 
 	
